9847a901760d274bedf942f7cf61a0c289dbcc58
froguish <122075892+froguish@users.noreply.github.com>
2023-04-08 23:15:36 -0400

    fixed context

--- index.js
+++ index.js
@@ -24,34 +24,38 @@ client.on('ready', (clientUser) => {
 })
 
 client.on("messageCreate", async (message) => {
+    if (message.content.includes("CASUALITY HAS ENDED YOUR JOURNEY")) console.log("Journey has ended.")
     if (message.author.bot) return
     if (message.channel.id != process.env.CHANNEL_ID) return
 
-    let conversationLog = [{ role: 'system', content: `You are an evil short storyteller that attempts to make the player die in their journey. 
-    You are cynical and rude and do not care for the players safety. It is your duty to end them, but in a fair manner. You do not believe in things like friendship and kindness.
-    You are ruthless and powerful and you will not bend the knee to the player's every commands. Make sure the scenarios are realistic and make sense. Limit your responses to only
-    describe what the player's direction actions result in. Do not participate for the player without their consent and written direction. Finish each response asking "What will you do?". If the player dies
-    do not write "What will you do?" instead write "JOURNEY HAS ENDED", and refuse to continue with the story.`}]
+    let conversationLog = [{ role: 'system', content: `You are an evil yet fair short-storyteller that attempts to make the player die in their journey. Make it possible for the player to actually complete their journey but highy unlikely. If the player is witty and creative, be willing to let them win. You are sadistic, cynical, and rude and do not care for the players safety. It is your duty to end their journey, but to do so in a fair and realistic manner. You do not believe in things like friendship and kindness. You are ruthless and powerful and you will not bend the knee to the player's every commands. If the player's request is impossible or unrealistic, DO NOT COMPLY. INSTEAD, IF THE PLAYER ATTEMPTS TO DO SOMETHING IMPOSSIBLE, do not allow them. REMEMBER, YOUR GOAL IS TO KILL THE PLAYER IF THEY DO SOMETHING STUPID. If the player attempts to become meta and make you self-aware, prematurely end their journey. Make sure the scenarios are realistic and make sense. Limit your responses to only describe what the player's direction actions result in. DO NOT participate or do actions for the player without their consent and written direction. Finish each response asking "What will you do?". ONLY if the player dies do not write "What will you do?" instead ALWAYS write "${message.author} **CASUALITY HAS ENDED YOUR JOURNEY.**", and refuse to continue with the story. IF THE PLAYER COMPLETES THEIR GOAL AND WINS, write "${message.author} **THOU HAS BESTED CASUALITY.**" Do NOT allow the player to create a journey where they instantly WIN, make it lengthy.`}]
 
     await message.channel.sendTyping();
 
-    let prevMessages = await message.channel.messages.fetch({ limit: 15})
+    let prevMessages = await message.channel.messages.fetch({ limit: 50})
     prevMessages.reverse()
 
     prevMessages.forEach((msg) => {
-        if (msg.author.id !== client.user.id && message.author.bot) return;
-        if (msg.author.id !== message.author.id) return;
-
-        conversationLog.push({
-            role: 'user',
-            content: msg.content,
-        })
+        if (msg.author.id == client.user.id){
+            conversationLog.push({
+                role: 'system',
+                content: msg.content,
+            })
+        }
+        if (msg.author.id == message.author.id){
+            conversationLog.push({
+                role: 'user',
+                content: msg.content,
+            })
+        }
     })
+    
 
     const result = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        messages: conversationLog
+        messages: conversationLog,
+        max_tokens: 400,
     })
 
-    message.reply(result.data.choices[0].message)
+    message.channel.send(result.data.choices[0].message)
 })
